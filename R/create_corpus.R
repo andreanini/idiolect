4ca9e5961ce47ecb6ed05672bf8be263dd3225e6
Andrea Nini <and.nini@gmail.com>
2026-01-04 18:40:59 +0100

    clarify role of create_corpus() function #6

--- R/create_corpus.R
+++ R/create_corpus.R
@@ -4,7 +4,7 @@
 #'
 #' @param path A string containing the path to a folder of plain text files (ending in .txt) with their name structured as following: authorname_textname.txt (e.g. smith_text1.txt).
 #'
-#' @return A `quanteda` corpus object with the authors' names as a docvar.
+#' @return A `quanteda` corpus object with the authors' names and the text names as docvars.
 #'
 #' @examples
 #' \dontrun{
@@ -15,8 +15,10 @@
 #' @export
 create_corpus <- function(path){
 
-  corpus <- readtext::readtext(file = paste0(path, "/*.txt"),
-                               docvarsfrom = "filenames", docvarnames = c("author", "textname")) |>
+  corpus <- readtext::readtext(
+    file = paste0(path, "/*.txt"),
+    docvarsfrom = "filenames", docvarnames = c("author", "textname")
+  ) |>
     quanteda::corpus()
 
   return(corpus)
